a3252878cfb8862c7ebfd204bba04c5342e032bf
lvbin <77638552@qq.com>
2018-09-27 20:00:35 +0800

    2018-09-27

--- src/main/java/com/lvbin/server/ServerController.java
+++ src/main/java/com/lvbin/server/ServerController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.TimeZone;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ServerController {
     private Logger logger = LoggerFactory.getLogger(ServerController.class);
 
@@ -21,6 +24,10 @@ public class ServerController {
     @RequestMapping(value = "/ip", method = RequestMethod.POST)
     @ResponseBody
     private String updateIP(HttpServletRequest httpServletRequest, @RequestBody String s) throws IOException {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        SimpleDateFormat bjSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");     // 北京
+        bjSdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));  // 设置北京时区
+
         String result = "failed";
         ObjectMapper objectMapper = new ObjectMapper();
         Map jsonObject =  objectMapper.readValue(s, Map.class);
@@ -29,9 +36,10 @@ public class ServerController {
             String remoteAddr = httpServletRequest.getRemoteAddr();
             int remotePort = httpServletRequest.getRemotePort();
             Date time = new Date();
+
             sysUserService.updateIP(remoteAddr, Integer.toString(remotePort), time);
             String ipaddr = String.format("%s:%d@%s", remoteAddr, remotePort, time.toString());
-            logger.warn(ipaddr);
+            //logger.warn(ipaddr);
             result = "success";
         }
         return result;
@@ -39,7 +47,9 @@ public class ServerController {
     @RequestMapping(value = "/ip", method = RequestMethod.GET)
     @ResponseBody
     private IPAddress GetIP(){
-        return sysUserService.getIP();
+        IPAddress ipAddress = sysUserService.getIP();
+        logger.warn(ipAddress.getTime().toString());
+        return ipAddress;
     }
 
     @RequestMapping(value = "/select", method = RequestMethod.GET)
